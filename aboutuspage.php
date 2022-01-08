f0da3129147b3ccaf076615a24e60a0832d663db
ceejayps <59537158+ceejayps@users.noreply.github.com>
2022-01-08 06:21:00 -0500

    Create aboutuspage.php

---
+++ aboutuspage.php
@@ -0,0 +1,36 @@
+<?php
+header('Access-Control-Allow-Origin: *');
+
+header('Access-Control-Allow-Methods: GET, POST');
+
+header("Access-Control-Allow-Headers: X-Requested-With");
+
+$servername = "mysqlcluster14";
+$username = "chenova";
+$password = "Chenova2020";
+$dbname = "chenova";
+
+// Create connection
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+// Check connection
+if (!$conn) {
+  die("Connection failed: " . mysqli_connect_error());
+}
+
+$sql = "SELECT id, Title, Body FROM AboutUsPage ";
+$result = mysqli_query($conn, $sql);
+
+if (mysqli_num_rows($result) > 0) {
+    $arr = [];
+    
+    while ($row = $result->fetch_assoc()) {
+        # code...
+        $jsonArrayObject = (array('id' => $row["id"], 'Title' => $row["Title"], "Body" => $row[`Body`]));
+        $arr[] = $jsonArrayObject;
+        
+    }
+    $json_array = json_encode($arr);
+    echo $json_array;
+} else {
+  echo "0 results";
+}
