c5e05bd66b9e850d42e4a21d1d34441bca7ef4ff
ceejayps <59537158+ceejayps@users.noreply.github.com>
2022-01-08 06:21:12 -0500

    Create updateable.php

---
+++ updateable.php
@@ -0,0 +1,39 @@
+<?php 
+header('Access-Control-Allow-Origin: *');
+
+header('Access-Control-Allow-Methods: GET, POST');
+
+header("Access-Control-Allow-Headers: X-Requested-With");
+
+$servername = "mysqlcluster14";
+$username = "chenova";
+$password = "Chenova2020";
+$dbname = "chenova";
+$id = 1;
+$Title;
+$body;
+$table;
+$name = $_POST['name'];
+$address= $_POST['address'];
+$email= $_POST['email'];
+$phone= $_POST['phone'];
+
+// Create connection
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+// Check connection
+if (!$conn) {
+  die("Connection failed: " . mysqli_connect_error());
+}
+
+
+$queryName = "UPDATE `contacts` SET `address` = '".$address."', `email` = '".$email."', `phone` = '".$phone."'  WHERE `id` = '".$id."'";
+
+$statement =$conn->prepare($queryName);
+$statement->execute();
+
+echo json_encode("Name Updated")
+
+
+//, `address` = '".$address."', `email` = '".$email."', `phone` = '".$phone."'
+
+?>
